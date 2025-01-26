bbfe32dc6ce41b86742a36d6a09e8a8172163e45
vanaigr <vanaigranov@gmail.com>
2025-01-26 17:17:43 -0600

    report location if shader/program error

--- src/defs.h
+++ src/defs.h
@@ -3,3 +3,9 @@
 
 void check_error(char const *file, int line);
 #define ce check_error(__FILE__, __LINE__)
+
+void shaderReport_(char const *file, int line, int shader, char const *name = "whatever");
+void programReport_(char const *file, int line, int prog, char const *name = "whatever");
+
+#define shaderReport(...) shaderReport_(__FILE__, __LINE__, __VA_ARGS__)
+#define programReport(...) programReport_(__FILE__, __LINE__, __VA_ARGS__)
--- src/gl.cpp
+++ src/gl.cpp
@@ -2,6 +2,8 @@
 #include<GL/glx.h>
 #include<stdio.h>
 
+#include"alloc.h"
+
 void check_error(char const *file, int line) {
     int err = glGetError();
     if(err) {
@@ -10,3 +12,28 @@ void check_error(char const *file, int line) {
         throw 1;
     }
 }
+
+void shaderReport_(char const *file, int line, GLint shader, char const *name) {
+    GLint res;
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &res);
+    if(!res) {
+        GLint len;
+        glGetShaderInfoLog(shader, tmp_end - tmp, &len, tmp);
+        printf("[%s %d] %s shader said %.*s", file, line, name, len, tmp);
+        fflush(stdout);
+        throw 1;
+    }
+}
+
+void programReport_(char const *file, int line, GLint prog, char const *name) {
+    GLint res;
+    glGetProgramiv(prog, GL_LINK_STATUS, &res);
+    if(!res) {
+        GLint len;
+        glGetProgramInfoLog(prog, tmp_end - tmp, &len, tmp);
+        printf("[%s %d] %s program said %.*s", file, line, name, len, tmp);
+        fflush(stdout);
+        throw 1;
+    }
+}
+
